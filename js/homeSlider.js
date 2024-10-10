93695c858982ce340f705b9cb566cbae1cca4070
zhngtrrz <jasminezhainegutierrez@gmail.com>
2024-10-10 14:48:29 +0800

    first version

---
+++ js/homeSlider.js
@@ -0,0 +1,12 @@
+var slider = tns({
+  container: ".homeSlider__img",
+  items: 6,
+  autoHeight: true,
+  speed: 700,
+  slideBy: "1",
+  autoplay: false,
+  navPosition: "bottom",
+  containerControls: "#controls",
+  prevButton: ".prev",
+  nextButton: ".next",
+});
